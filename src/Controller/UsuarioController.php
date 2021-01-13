ab68097b6f11e84618f69c557580e2561c3c4084
MarcosBlua <marcos.blua@hotmail.com>
2021-01-13 13:54:06 -0300

    Controlador usuario

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\UserType;
+
+class UsuarioController extends AbstractController
+{
+    /**
+     * @Route("/usuario", name="usuario")
+     */
+    public function index(): Response
+    {
+        return $this->render('usuario/index.html.twig', [
+            'controller_name' => 'UsuarioController',
+        ]);
+    }
+
+    /**
+     * @Route("/perfil", name="perfil")
+     */
+}
